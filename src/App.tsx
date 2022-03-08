042a7e36bc7cab5e3fa3b7aa22f897a5a860d2de
Valeska Cheryl <valeskacheryl@gmail.com>
2022-03-08 13:08:02 +0700

    users and posts data commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,101 @@
 import React from 'react';
-import logo from './logo.svg';
+import { API_URL } from './components/consts'
+import { apiCall } from './components/services'
+import { IUserRes, IPostRes } from './components/interface'
 import './App.css';
+// import logo from './logo.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IUsersPosts extends IUserRes {
+  posts: IPostRes[]
+}
+
+interface IAppState {
+    allData: IUsersPosts[]
+}
+
+class App extends React.Component<{}, IAppState> {
+    constructor(props: {}) {
+        super(props);
+        this.state = {
+            allData: [],
+        }
+    }
+
+    componentDidMount() {
+        console.log('Did Mount')
+        apiCall(API_URL.users)
+            .then((data: any) => {
+
+                let users: IUsersPosts[] = data;
+                
+                users.forEach((user: any) => {
+                    let urls = API_URL.posts.replace(`{userId}`, user.id.toString());
+                    console.log(urls);
+
+                    apiCall(urls)
+                        .then((posts) => {
+                            const { allData } = this.state;
+                            user.posts = posts as IPostRes[]
+
+                            allData.push(user)
+
+                            this.setState({allData: allData})
+                        })
+                        .catch((err) => {
+                            console.log(err + 'post error');
+                        })
+                });
+            })
+            .catch((error) => {
+                console.log(error + 'user error')
+            })
+            .finally(() => {
+                console.log('done fetching')
+            })
+        
+        console.log("all data: ", this.state.allData)
+    }
+
+    componentDidUpdate() {
+        console.log('Did Update')
+    }
+
+    renderPosts = (posts: IPostRes[]) => {
+        return posts.map((post) => {
+            return (
+                <div key={post.id}>
+                    <h4>{post.title}</h4>
+                    <p>{post.body}</p>
+                </div>
+            )
+        })
+    }
+
+    renderUsers = () => {
+        const { allData } = this.state;
+        return allData.map((user) => {
+            return (
+                <div key={user.id}>
+                    <br></br>
+                    <h2>{user.id}</h2>
+                    <h1>{user.name}</h1>
+                    <h2>{user.username}</h2>
+                    <h2>{user.email}</h2>
+                    {this.renderPosts(user.posts)}
+                    <br></br>
+                </div>
+            )
+        })
+    }
+
+    render() {
+        console.log("render called ...")
+        return (
+        <div className="app-style">
+            {this.renderUsers()}
+        </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/consts.ts
@@ -0,0 +1,4 @@
+export const API_URL = {
+    users: 'https://jsonplaceholder.typicode.com/users',
+    posts: 'https://jsonplaceholder.typicode.com/posts?userId={userId}'
+}
